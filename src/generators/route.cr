109eb942ad75857a30114f8575a28f1b9c691a8e
Jamie Gaskins <jgaskins@hey.com>
2023-11-29 22:12:10 -0600

    Add initial route implementation

---
+++ src/generators/route.cr
@@ -0,0 +1,49 @@
+require "./generator"
+
+module Wax::Generators
+  class Route < Generator
+    handle "route"
+
+    getter name : String
+
+    def self.description
+      "Generate an Armature route"
+    end
+
+    def self.new(args : Array(String))
+      new args.first
+    end
+
+    def initialize(@name)
+    end
+
+    def call
+      file "src/routes/#{@name.underscore}.cr", <<-EOF
+        require "./route"
+
+        struct #{@name.camelcase(lower: false)}
+          include Route
+
+          def call(context)
+            route context do |r, response, session|
+              r.root do
+                r.get do
+                  # ...
+                end
+
+                r.post do
+                  # ...
+                end
+              end
+
+              r.on id: UUID do |id|
+                # Make sure you use HTTP methods like `r.get` or `r.post` to indicate
+                # a terminal route for Armature.
+              end
+            end
+          end
+        end
+        EOF
+    end
+  end
+end
