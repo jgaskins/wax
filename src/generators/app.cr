f055a8fb9df374d8e0727c093a6131c7f6025a74
Jamie Gaskins <jgaskins@hey.com>
2024-11-10 19:24:36 -0600

    Use Crystal 0.14.0 in the Dockerfile

--- src/generators/app.cr
+++ src/generators/app.cr
@@ -1046,7 +1046,7 @@ module Wax::Generators
 
     def dockerfile
       file "Dockerfile", <<-EOF
-        FROM 84codes/crystal:1.12.2-alpine AS builder
+        FROM 84codes/crystal:1.14.0-alpine AS builder
 
         COPY shard.yml shard.lock /app/
         WORKDIR /app
