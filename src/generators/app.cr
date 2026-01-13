8a986fc6d5cafd09186008fd92f275bf2cbca4e2
Jamie Gaskins <jgaskins@hey.com>
2026-01-12 23:47:08 -0500

    Fix syntax error in button generator

--- src/generators/app.cr
+++ src/generators/app.cr
@@ -1058,7 +1058,7 @@ module Wax::Generators
           end
 
           def submit(message : String = "Save")
-            Button.new message type: :submit
+            Button.new message, type: :submit
           end
 
           def attributes
